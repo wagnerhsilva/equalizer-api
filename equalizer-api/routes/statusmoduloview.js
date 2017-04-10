09f85c7c5004eabbb1480cf922655819d4577c2c
Rodrigo Salles Bueno <rodrigo.bueno@phiinnovations.com>
2017-04-09 21:26:02 -0300

    status name view

--- equalizer-api/routes/statusmoduloview.js
+++ equalizer-api/routes/statusmoduloview.js
@@ -11,6 +11,6 @@ var isAuthenticated = function (req, res, next) {
 }
 /* GET home page. */
 router.get('/', isAuthenticated,  function (req, res, next) {
-    res.render('statusModuloView', { title: 'Status dos Módulos', pageName: 'statusmoduloview', username: req.user.nome, userAccess: req.user.acesso, userEmail: req.user.email });
+    res.render('statusmoduloview', { title: 'Status dos Módulos', pageName: 'statusmoduloview', username: req.user.nome, userAccess: req.user.acesso, userEmail: req.user.email });
 });
 module.exports = router;
