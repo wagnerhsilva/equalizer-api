330b0938e445b9e2731a3374c097ac588dcde79c
Rodrigo Bueno <rodrigo.bueno@softsharp.com.br>
2017-08-16 14:19:49 -0300

    Botões: Reiniciar e Ligar/Desligar SNMP

--- equalizer-api/routes/parameterview.js
+++ equalizer-api/routes/parameterview.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var setup = require('setup')();
 var router = express.Router();
 var isAuthenticated = function (req, res, next) {
     // if user is authenticated in the session, call the next() to call the next request handler
@@ -13,5 +14,65 @@ var isAuthenticated = function (req, res, next) {
 router.get('/', isAuthenticated,  function (req, res, next) {
     res.render('parameterview', { title: 'Parâmetros', pageName: 'parameterview', username: req.user.nome, userAccess: req.user.acesso, userEmail: req.user.email, showHeaderData: global.showHeaderInfo });
 });
-
+router.get('/reboot', isAuthenticated,  function (req, res, next) {
+    console.log("Início função reboot.");
+    var exec = require('child_process').exec;
+    var cmd = 'sudo reboot';
+    exec(cmd, function (error, stdout, stderr) {
+        if (error) {
+            console.error(error);
+            res.json("Erro ao reiniciar.");
+            return;
+        }
+        console.log("efetuado");
+        console.log(stdout);
+        res.json("efetuado.");
+    });
+});
+router.get('/onSNMP', isAuthenticated,  function (req, res, next) {
+    console.log("Início função On SNMP.");
+    var exec = require('child_process').exec;
+    var cmd = '/etc/init.d/snmpd start';
+    exec(cmd, function (error, stdout, stderr) {
+        if (error) {
+            console.error(error);
+            res.send("Erro ao executar '/etc/init.d/snmpd start.");
+            return;
+        }
+        cmd = '/etc/init.d/equalizer-traps start';
+        exec(cmd, function (error, stdout, stderr) {
+            if (error) {
+                console.error(error);
+                res.send("Erro ao executar equalizer-traps start.");
+                return;
+            }
+            console.log("efetuado");
+            console.log(stdout);
+            res.send("efetuado.");
+        });
+    });
+});
+router.get('/offSNMP', isAuthenticated,  function (req, res, next) {
+    console.log("Início função Off SNMP.");
+    var exec = require('child_process').exec;
+    var cmd = '/etc/init.d/snmpd stop';
+    exec(cmd, function (error, stdout, stderr) {
+        if (error) {
+            console.error(error);
+            res.send("Erro ao executar '/etc/init.d/snmpd stop.");
+            return;
+        }
+        cmd = '/etc/init.d/equalizer-traps stop';
+        exec(cmd, function (error, stdout, stderr) {
+            if (error) {
+                console.error(error);
+                res.send("Erro ao executar equalizer-traps stop.");
+                return;
+            }
+            console.log("efetuado");
+            console.log(stdout);
+            res.send("efetuado.");
+        });
+    });
+});
 module.exports = router;
