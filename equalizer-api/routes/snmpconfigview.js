3fa6032d5401c61f2c746d367106ce143763a9cf
Flavio Alves <flavio.alves@phiinnovations.com>
2019-11-07 07:59:06 -0300

    Correção problemas encontrados na implementação #33

--- equalizer-api/routes/snmpconfigview.js
+++ equalizer-api/routes/snmpconfigview.js
@@ -12,6 +12,7 @@ var isAuthenticated = function (req, res, next) {
 }
 /* GET home page. */
 router.get('/', isAuthenticated, function (req, res, next) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
     res.render('snmpconfigview', { title: 'Configurações de SNMP', pageName: 'snmpconfigview', username: req.user.nome, userAccess: req.user.acesso, userEmail: req.user.email, showHeaderData: global.showHeaderInfo, headerInfoCDec: global.headerInfoCDec, translate: res.__('snmpconfig'), translate_header: res.__('header') });
 });
 router.get('/showHideHeaderInfo', function (req, res, next) {
